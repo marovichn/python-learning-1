19dbf8ec145fb3454715555e2ac08a790e6d00ff
marovichn <nikolamarovic.11@gmail.com>
2024-08-12 19:19:06 +0200

    Python modules learning

---
+++ main.py
@@ -0,0 +1,34 @@
+#import sys
+#import random
+
+#sys.argv
+
+#random_number = random.randint(1, 10)
+
+#while True:
+#    try:
+#        guess = int(input('guess a number 1~10:'))
+#        if 0 < guess < 11:
+#            if guess == random_number:
+#                print("congrats you are genius")
+#            else:
+#                print("not true")
+#                continue
+#            break
+#        else:
+#            print('hey bozo, I said 1~10')
+#   except ValueError:\
+#        print('please enter a number')
+#        continue
+
+
+#import pyjokes
+
+#while True:
+#    inputs = input("press key")
+#    if inputs:
+#        joke = pyjokes.get_joke("en", "neutral")
+#        print(joke)
+#        continue
+
+
